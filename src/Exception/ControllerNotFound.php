32a97ef4cbedd5fc6f38494482ae081907e9ad78
milan-miscevic <milan.miscevic@gmail.com>
2020-03-12 10:44:26 +0100

    The ControllerNotFound exception is removed (unused)

--- src/Exception/ControllerNotFound.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Inert\Exception;
-
-class ControllerNotFound extends \RuntimeException implements ExceptionInterface
-{
-    protected $message = 'Controller can not be found.';
-}
