155ad9c9514f5fd303ab473d9301dfbfa174256f
milan-miscevic <milan.miscevic@gmail.com>
2020-07-07 22:28:59 +0200

    Add ErrorActionTest

---
+++ tests/ErrorActionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inert\Tests;
+
+use Inert\ErrorAction;
+use PHPUnit\Framework\TestCase;
+
+class ErrorActionTest extends TestCase
+{
+    private const ERROR_MESSAGE = 'This is an error page.';
+
+    public function testErrorPage(): void
+    {
+        $action = new ErrorAction(new \Exception());
+        $action->setViewFolder(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'view');
+
+        ob_start();
+        $action->run();
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertSame(static::ERROR_MESSAGE, $content);
+    }
+}
