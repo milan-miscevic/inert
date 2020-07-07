12bc86378093a7e89615ad0b1ae5150b869b2d2f
milan-miscevic <milan.miscevic@gmail.com>
2020-07-07 20:20:32 +0200

    Clean-up of the DependentService test class

--- tests/Sample/DependentService.php
+++ tests/Sample/DependentService.php
@@ -6,10 +6,7 @@ namespace Inert\Tests\Sample;
 
 class DependentService
 {
-    private SimpleService $simpleService;
-
     public function __construct(SimpleService $simpleService)
     {
-        $this->simpleService = $simpleService;
     }
 }
