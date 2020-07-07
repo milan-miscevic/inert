a19ac768f0171c1889495535757eb492c0de5bea
milan-miscevic <milan.miscevic@gmail.com>
2020-07-07 23:31:19 +0200

    Add ApplicationTest

---
+++ tests/ApplicationTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inert\Tests;
+
+use Inert\ActionLocator;
+use Inert\Application;
+use Inert\BaseAction;
+use PHPUnit\Framework\TestCase;
+
+class ApplicationTest extends TestCase
+{
+    private const ERROR_MESSAGE = 'This is a text.';
+
+    public function testActionSuccessful(): void
+    {
+        $stub = $this->getMockBuilder(ActionLocator::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $callback = function () {
+            return new class extends BaseAction {
+                public function run(): void
+                {
+                    echo 'This is a text.';
+                }
+            };
+        };
+
+        $stub->method('get')
+            ->willReturnCallback($callback);
+
+        $application = new Application($stub);
+
+        ob_start();
+        $application->run();
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertSame(static::ERROR_MESSAGE, $content);
+    }
+}
