ae26b126ccfbc9f40d86930b42f324788c747fdf
milan-miscevic <milan.miscevic@gmail.com>
2020-07-08 16:06:12 +0200

    Add a test if an action is not found, reach 100 % coverage

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -7,21 +7,23 @@ namespace Inert\Tests;
 use Inert\ActionLocator;
 use Inert\Application;
 use Inert\BaseAction;
+use Inert\Exception\ActionNotFound;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class ApplicationTest extends TestCase
 {
-    private const ERROR_MESSAGE = 'This is a text.';
+    private const ERROR_MESSAGE = 'This is an error page.';
+    private const SUCCESSFUL_MESSAGE = 'This is a text.';
 
     public function testActionSuccessful(): void
     {
         /** @var ActionLocator&MockObject */
-        $stub = $this->getMockBuilder(ActionLocator::class)
+        $actionLocator = $this->getMockBuilder(ActionLocator::class)
             ->disableOriginalConstructor()
             ->getMock();
 
-        $callback = function () {
+        $action = function () {
             return new class extends BaseAction {
                 public function run(): void
                 {
@@ -30,10 +32,31 @@ class ApplicationTest extends TestCase
             };
         };
 
-        $stub->method('get')
-            ->willReturnCallback($callback);
+        $actionLocator->method('get')
+            ->willReturnCallback($action);
 
-        $application = new Application($stub, '');
+        $application = new Application($actionLocator, '');
+
+        ob_start();
+        $application->run();
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        $this->assertSame(static::SUCCESSFUL_MESSAGE, $content);
+    }
+
+    public function testActionNotFound(): void
+    {
+        /** @var ActionLocator&MockObject */
+        $actionLocator = $this->getMockBuilder(ActionLocator::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $actionLocator->method('get')
+            ->willThrowException(new ActionNotFound());
+
+        $viewFolder = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'view';
+        $application = new Application($actionLocator, $viewFolder);
 
         ob_start();
         $application->run();
